8f06528411663c2f1725bd38167a65478a1bdbe4
moonjiyoung <umarg228@naver.com>
2023-04-12 17:38:21 +0900

    2222

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactdfdfdfdfddf
         </a>
       </header>
     </div>
